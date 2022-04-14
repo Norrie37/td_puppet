ac05d0826027f53cd7a36c90a5e3a0375984849d
JDUNA <jduna.local.com>
2022-04-14 13:59:13 +1000

    Added a readme file for puppet-master server

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,9 @@
 node default {
-  
+  file { '/root/README':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0664',
+    content => 'This puppet master server hosts and manages vm-agents.',
+  }
 }
