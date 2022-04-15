0463695485b0ad6d434dadaa53872980a7a0e49a
JDUNA <jduna.local.com>
2022-04-15 10:03:13 +1000

    Syntax Refactoring

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,10 +4,10 @@ node default {
 
 node "centos7-host.subnet04030040.vcn04030040.oraclevcn.com" {
   file { '/root/README':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0664',
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0664',
     content => 'This puppet master server hosts and manages vm-agents.',
   }
   include role::master_server
@@ -15,10 +15,10 @@ node "centos7-host.subnet04030040.vcn04030040.oraclevcn.com" {
 
 node /^web/ {
   file { '/root/README':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0664',
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0664',
     content => 'This web agent hosts the nginx service.',
   }
   include role::web_server
